7e24dc910abc3d10dab321301c3a5e4ac6db9561
MikhailYuzhakov <miha.yuzhakov@gmail.com>
2022-05-28 11:56:21 +0300

    Генератор двумерного массива

---
+++ Task_1/Program.cs
@@ -0,0 +1,21 @@
+﻿/*
+Задача 47: Задайте двумерный массив размером m×n, заполненный случайными вещественными числами.
+*/
+
+double[,] array = new double[1000,1000];
+int i = 0, j = 0;
+Random rand = new Random();
+
+Console.Clear();
+Console.WriteLine("Введите размерность массива MxN с клавиатуры через пробел: ");
+int[] size = Console.ReadLine().Split().Select(int.Parse).ToArray();
+
+for (i = 0; i < size[1]; i++)
+    for (j = 0; j < size[0]; j++)
+        array[i,j] = rand.NextDouble();
+
+for (i = 0; i < size[1]; i++) {
+    for (j = 0; j < size[0]; j++)
+        Console.Write(Math.Round(array[i,j], 2) + " ");
+    Console.WriteLine();
+}
